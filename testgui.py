78473419d45db0b030119becd59728c2652e07a5
Jeremy <chng_jeremy@outlook.com>
2021-09-27 13:34:43 +0800

    changed abit of stuff

--- testgui.py
+++ testgui.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'testgui.ui'
-##
-## Created by: Qt User Interface Compiler version 5.15.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
 from PySide2.QtCore import *
